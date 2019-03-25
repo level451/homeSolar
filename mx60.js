e1debd8afd8bc6b9a1d50eaabbb853eb385e84de
Todd <twitzel@gmail.com>
2019-03-25 13:47:51 -0600

    added baud

--- mx60.js
+++ mx60.js
@@ -2,8 +2,10 @@ const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
 const port = new SerialPort('/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0',
     {baudRate: 19200})
-
-const parser = port.pipe(new Readline({delimiter: '\r'}))
+port.on('data', function (data) {
+    console.log('Data:', data)
+})
+//const parser = port.pipe(new Readline({delimiter: '\r'}))
 console.log('here');
 parser.on('data', console.log)
 port.on('open',function(){console.log('open')})
