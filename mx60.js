b557dd849ff3614b3496c2269e9b6e5da4d5547c
Todd <twitzel@gmail.com>
2019-03-25 15:41:12 -0600

    added baud

--- mx60.js
+++ mx60.js
@@ -72,7 +72,7 @@ port.on('open',function(){
             }
 
             if ( Math.abs(lastData[outbackData[0]].batteryVoltage - outbackObject.batteryVoltage) > 0.1 ){
-                console.log(outbackData[0], ": ", lastData[outbackData[0]].batteryVoltage, lastData[outbackData[0]].batteryVoltage - outbackObject.batteryVoltage,Math.abs(lastData[outbackData[0]].batteryVoltage - outbackObject.batteryVoltage))
+                console.log(outbackData[0], ": ", lastData[outbackData[0]].batteryVoltage, lastData[outbackData[0]].batteryVoltage - outbackObject.batteryVoltage, typeof Math.abs(lastData[outbackData[0]].batteryVoltage - outbackObject.batteryVoltage))
                 lastData[outbackData[0]] = outbackObject
                 mx60Emitter.emit('data',lastData)
             } else
