5af4a245ac183e801dff8b52a82ab75b66959011
Todd <twitzel@gmail.com>
2019-05-02 08:49:51 -0600

    added baudd

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-loadLocalSettings()
-mx60 = require('./mx60')
-mx60.test('=================')
-
-
+loadLocalSettings();
+mx60 = require('./mx60');
+mx60.test('=================');
 const connector = require('@level451/connector');
-connector.connect(localSettings.home.address,true,'MX60','mx60');
-
+connector.connect(localSettings.home.address, true, 'MX60', 'mx60');
 // function loadLocalSettings() {
 //     try {
 //         global.localSettings = require('./localSettings.JSON')
@@ -15,9 +12,14 @@ connector.connect(localSettings.home.address,true,'MX60','mx60');
 //     }
 // }
 function loadLocalSettings() {
-global.localSettings = {
-    "home":{
-        "address":"level451.com:2112"
-    }
-}
+    global.localSettings =
+        {
+            home: {
+                address: "10.6.1.2:2112"
+            },
+            ServiceInfo: {
+                id: "mx60",
+                systemType: "solar"
+            }
+        };
 }
